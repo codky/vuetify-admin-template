61b31fd723df5046a50429a15e2da03990ecf6de
codky <msky0912@gmail.com>
2024-01-04 14:00:55 +0900

    lazy loading

--- src/router/index.js
+++ src/router/index.js
@@ -1,99 +1,99 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Dashboard from "@/views/Dashboard.vue"
-import GridSystem from "@/views/GridSystem.vue"
-import GridListPage from "@/views/GridListPage.vue"
-import Breakpoints from "@/views/Breakpoints.vue"
-import Typography from "@/views/Typography.vue"
-import Tables from "@/views/Tables.vue"
-import Forms from "@/views/Forms.vue"
-import Buttons from "@/views/Buttons.vue"
-import Icons from "@/views/Icons.vue"
-import SignIn from "@/views/authentication/SignIn.vue"
-import SignUp from "@/views/authentication/SignUp.vue"
-import ProductList from "@/views/page/ProductList.vue"
-import DefaultLayout from "@/layouts/default/Index.vue"
-import PageLayout from "@/layouts/page/Index.vue"
-import AuthenticationLayout from "@/layouts/authentication/Index.vue"
+// import Dashboard from "@/views/Dashboard.vue"
+// import GridSystem from "@/views/GridSystem.vue"
+// import GridListPage from "@/views/GridListPage.vue"
+// import Breakpoints from "@/views/Breakpoints.vue"
+// import Typography from "@/views/Typography.vue"
+// import Tables from "@/views/Tables.vue"
+// import Forms from "@/views/Forms.vue"
+// import Buttons from "@/views/Buttons.vue"
+// import Icons from "@/views/Icons.vue"
+// import SignIn from "@/views/authentication/SignIn.vue"
+// import SignUp from "@/views/authentication/SignUp.vue"
+// import ProductList from "@/views/page/ProductList.vue"
+// import DefaultLayout from "@/layouts/default/Index.vue"
+// import PageLayout from "@/layouts/page/Index.vue"
+// import AuthenticationLayout from "@/layouts/authentication/Index.vue"
 
 Vue.use(VueRouter)
 
 const routes = [
     {
       path: '/',
-      component: DefaultLayout,
+      component: () => import(/* webpackChunkName: "layouts-default-index" */'@/layouts/default/Index.vue'),
       children: [
-          {
+        {
             path: '/',
             name: 'Dashboard',
-            component: Dashboard
+            component: () => import(/* webpackChunkName: "views-dashboard" */'@/views/Dashboard.vue')
         },
         {
             path: '/grid-system',
             name: 'GridSystem',
-            component: GridSystem
+            component: () => import(/* webpackChunkName: "views-grid-system" */'@/views/GridSystem.vue')
         },
         {
             path: '/grid-list-page',
             name: 'GridListPage',
-            component: GridListPage
+            component: () => import(/* webpackChunkName: "views-grid-list-page" */'@/views/GridListPage.vue')
         },
         {
           path: '/breakpoints',
           name: 'Breakpoints',
-          component: Breakpoints
+          component: () => import(/* webpackChunkName: "views-breakpoints" */'@/views/Breakpoints.vue')
         },
         {
           path: '/typography',
           name: 'Typography',
-          component: Typography
+          component: () => import(/* webpackChunkName: "views-typography" */'@/views/Typography.vue')
         },
         {
           path: '/tables',
           name: 'Tables',
-          component: Tables
+          component: () => import(/* webpackChunkName: "views-tables" */'@/views/Tables.vue')
         },
           {
           path: '/forms',
           name: 'Forms',
-          component: Forms
+          component: () => import(/* webpackChunkName: "views-forms" */'@/views/Forms.vue')
         },
         {
           path: '/buttons',
           name: 'Buttons',
-          component: Buttons
+          component: () => import(/* webpackChunkName: "views-buttons" */'@/views/Buttons.vue')
         },
         {
           path: '/icons',
           name: 'Icons',
-          component: Icons
+          component: () => import(/* webpackChunkName: "views-icons" */'@/views/Icons.vue')
         },
       ]
     },
     {
       path: '/authentication',
-      component: AuthenticationLayout,
+      component: () => import(/* webpackChunkName: "views-authentication-index" */'@/layouts/authentication/Index.vue'),
       children: [
         {
           path: 'sign-in',
           name: 'signIn',
-          component: SignIn
+          component: () => import(/* webpackChunkName: "views-sign-in" */'@/views/authentication/SignIn')
         },
         {
           path: 'sign-up',
           name: 'signUp',
-          component: SignUp
+          component: () => import(/* webpackChunkName: "views-sign-up" */'@/views/authentication/SignUp')
         },
       ]
     },
     {
       path: '/page',
-      component: PageLayout,
+      component: () => import(/* webpackChunkName: "views-page-index */'@/layouts/page/Index.vue'),
       children: [
         {
           path: 'product-list',
           name: 'productList',
-          component: ProductList
+          component: () => import(/* webpackChunkName: "views-page-product-list */'@/views/page/ProductList.vue'),
         },
       ]
     }
